39e09bc143418ef550587d5690271de5c364fb80
Andres de Lago <delagoandres@gmail.com>
2017-09-28 10:16:14 -0500

    Adds Tweets file/class for easier handling of tweet objects

---
+++ Tweets.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import json
+
+
+class Tweet:
+
+    emoji = []
+    text = ""
+    creation_date = None
+    id = -1
+    user = None
+
+    def __init__(self, data):
+        if 'emoji' in data:
+            self.emoji = data['emoji']
+        if 'text' in data:
+            self.text = data['text']
+        if 'created_at' in data:
+            self.creation_date = data['created_at']
+        if 'id' in data:
+            self.id = data['id']
+        if 'user' in data:
+            self.user = User(data['user'])
+
+
+class User:
+    username = ''
+    id = -1
+
+    def __init__(self, data):
+        if 'id' in data:
+            self.id = data['id']
+        if 'user_name' in data:
+            self.username = data['user_name']
+
+
+def read_json_file(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)['tweets']
+    return [Tweet(obj) for obj in data]
